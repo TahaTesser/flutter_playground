4c3d0ada9cc0bd623371b577d7289fe829038673
Taha Tesser <tessertaha@gmail.com>
2024-08-19 18:43:01 +0300

    Tests check

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,1 +1,46 @@
-void main() { }
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+
+    // This is a regression test for https://github.com/flutter/flutter/issues/153544.
+  testWidgets('Tooltip is drawn when hovering within IconButton area but outside the Icon tself', (WidgetTester tester) async {
+    const String tooltipText = 'Test tooltip';
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Material(
+          child: Center(
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.ac_unit),
+              tooltip: tooltipText,
+            ),
+          ),
+        ),
+      ),
+    );
+
+    Finder findTooltipContainer(String tooltipText) {
+      return find.ancestor(
+        of: find.text(tooltipText),
+        matching: find.byType(Container),
+      );
+    }
+
+    // Verify that the tooltip is not shown initially.
+    expect(findTooltipContainer(tooltipText), findsNothing);
+
+    // Start hovering within IconButton area to show the tooltip.
+    final TestGesture gesture = await tester.createGesture(kind: PointerDeviceKind.mouse);
+    await gesture.addPointer();
+
+    final Offset topLeft = tester.getTopLeft(find.byType(Icon));
+    // print(topLeft);
+    await gesture.moveTo(Offset(topLeft.dx - 1, topLeft.dy - 1));
+    await tester.pumpAndSettle();
+
+    // Verify that the tooltip is shown.
+    expect(findTooltipContainer(tooltipText), findsOneWidget);
+  });
+ }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,59 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//           child: Row(
+//             spacing: 20.0,
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: <Widget>[
+//               IconButton(
+//                 padding: const EdgeInsets.all(20.0),
+//                 icon: const ColoredBox(
+//                   color: Color(0xFFFF0000),
+//                   child: Icon(Icons.add),
+//                 ),
+//                 onPressed: () {},
+//                 tooltip: 'Tooltip',
+//               ),
+//               IconButton.filled(
+//                 padding: const EdgeInsets.all(20.0),
+//                 icon: const ColoredBox(
+//                   color: Color(0xFFFF0000),
+//                   child: Icon(Icons.add),
+//                 ),
+//                 onPressed: () {},
+//                 tooltip: 'Tooltip',
+//               ),
+//               IconButton.filledTonal(
+//                 padding: const EdgeInsets.all(20.0),
+//                 icon: const ColoredBox(
+//                   color: Color(0xFFFF0000),
+//                   child: Icon(Icons.add),
+//                 ),
+//                 onPressed: () {},
+//                 tooltip: 'Tooltip',
+//               ),
+//               IconButton.outlined(
+//                 padding: const EdgeInsets.all(20.0),
+//                 icon: const ColoredBox(
+//                   color: Color(0xFFFF0000),
+//                   child: Icon(Icons.add),
+//                 ),
+//                 onPressed: () {},
+//                 tooltip: 'Tooltip',
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -8,12 +64,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Sample'),
-        ),
-      ),
+        home: const Material(child: Text('Home')),
+        routes: <String, WidgetBuilder>{
+          '/next': (BuildContext context) {
+            return const Material(
+              child: Center(
+                child: BackButton(),
+              ),
+            );
+          },
+        },
+
     );
   }
 }
